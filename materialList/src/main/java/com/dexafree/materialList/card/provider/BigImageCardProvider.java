607d98390465fd3ed634743cdbdf39e1de5042c4
wangwei-25 <51907228+wangwei-25@users.noreply.github.com>
2020-07-06 15:01:29 +0800

    Create BigImageCardProvider.java

---
+++ materialList/src/main/java/com/dexafree/materialList/card/provider/BigImageCardProvider.java
@@ -0,0 +1,12 @@
+package com.dexafree.materialList.card.provider;
+
+import com.dexafree.materialList.R.layout;
+
+public class BigImageCardProvider
+  extends TextCardProvider<BigImageCardProvider>
+{
+  public int getLayout()
+  {
+    return R.layout.material_big_image_card_layout;
+  }
+}
